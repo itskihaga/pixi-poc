35dc3c1a72d30fd13d40fe224beeb448cb625e35
haga-itsuki <haga-itsuki@dmm.com>
2020-10-31 12:42:35 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,51 @@ const app = new Application({
 if (elm) {
   elm.appendChild(app.view);
 }
+firebase.initializeApp({
+  apiKey: "AIzaSyAabuvSu6W3mebZOXbI9TVW59BLShbufU4",
+  projectId: "momomo-game",
+});
+const db = firebase.firestore();
+const isNewGame = confirm("new room?");
+const roomsCol = db.collection("rooms");
+
+interface PlayersRepository {
+  addPlayer(player: Player): Promise<void>;
+  fetchPlayers(): Promise<Player[]>;
+  fetchPlayersSize(): Promise<number>;
+  onOtherPlayerAdd(cb: (players: Player[]) => void): Promise<void>;
+}
+const createPlayersRepository = (
+  roomRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>
+): PlayersRepository => {
+  const playersRef = roomRef.collection("players");
+  const playersMap = new Map<string, Player>();
+  return {
+    async addPlayer(player: Player): Promise<void> {
+      playersMap.set(player.name, player);
+      await playersRef.add(player);
+    },
+    async fetchPlayers(): Promise<Player[]> {
+      const data = await playersRef.get();
+      return data.docs.map((p) => p.data() as Player);
+    },
+    async fetchPlayersSize(): Promise<number> {
+      const data = await playersRef.get();
+      return data.size;
+    },
+    async onOtherPlayerAdd(cb: (players: Player[]) => void) {
+      playersRef.onSnapshot((playersData) => {
+        const newPlayers = playersData.docs
+          .map((e) => e.data() as Player)
+          .filter((player) => !playersMap.has(player.name));
+        newPlayers.forEach((player) => {
+          playersMap.set(player.name, player);
+        });
+        cb(newPlayers);
+      });
+    },
+  };
+};
 
 const addListener = (target: Container) => {
   window.addEventListener("keypress", (e) => {
@@ -43,69 +88,48 @@ const createUnit = ({ x, y, color }: Player) => {
   return unit;
 };
 
-firebase.initializeApp({
-  apiKey: "AIzaSyAabuvSu6W3mebZOXbI9TVW59BLShbufU4",
-  projectId: "momomo-game",
-});
-const db = firebase.firestore();
-const isNewGame = confirm("new room?");
-const roomsCol = db.collection("rooms");
+const createNewPlayer = (name: string): Player => {
+  return {
+    x: Math.floor(Math.random() * 50),
+    y: Math.floor(Math.random() * 50),
+    color:
+      Math.floor(Math.random() * 256) +
+      Math.floor(Math.random() * 256) * 256 +
+      Math.floor(Math.random() * 256) * 256 * 256,
+    name,
+  };
+};
 
 const playersMap = new Map<string, Player>();
 
-const enterToRoom = (
-  roomRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>
-) => {
+const enterToRoom = (repo: PlayersRepository) => {
   const name = prompt("name?");
   if (name) {
-    const player: Player = {
-      x: Math.floor(Math.random() * 50),
-      y: Math.floor(Math.random() * 50),
-      color:
-        Math.floor(Math.random() * 256) +
-        Math.floor(Math.random() * 256) * 256 +
-        Math.floor(Math.random() * 256) * 256 * 256,
-      name,
-    };
+    const player: Player = createNewPlayer(name);
     const unit = createUnit(player);
     addListener(unit);
     playersMap.set(name, player);
-    roomRef
-      .collection("players")
-      .add({
-        ...player,
-      })
-      .then(() => {
-        roomRef.collection("players").onSnapshot((players) => {
-          players.forEach((p) => {
-            const data = p.data() as Player;
-            if (!playersMap.has(data.name)) {
-              playersMap.set(data.name, data);
-              createUnit(data);
-            }
-          });
-          console.log("update", players.size);
-        });
-      });
+    repo.addPlayer(player);
+    repo.onOtherPlayerAdd((players) => {
+      players.forEach(createUnit);
+    });
   }
 };
 
 if (isNewGame) {
   const roomRef = roomsCol.doc();
+  const repo = createPlayersRepository(roomRef);
   console.log(roomRef.id);
-  enterToRoom(roomRef);
+  enterToRoom(repo);
 } else {
   const roomid = prompt("room id?");
   if (roomid) {
     const roomRef = roomsCol.doc(roomid);
-    roomRef
-      .collection("players")
-      .get()
-      .then((players) => {
-        console.log("room players ", players.size);
-        if (players.size) {
-          enterToRoom(roomRef);
-        }
-      });
+    const repo = createPlayersRepository(roomRef);
+    repo.fetchPlayers().then((players) => {
+      if (players.length) {
+        enterToRoom(repo);
+      }
+    });
   }
 }
